e4f1b41f4901ddea012930b34648a38064fdb420
Feng Zhou <zhoufwind@hotmail.com>
2019-05-14 11:35:16 +0800

    <feature>: update FE and BE Dockerfile and k8s yaml
    BE
    - Change PVC settings back to main yaml file
    - execute BE as non-root user, run as palo
    
    FE
    - Create FE Dockerfile
    - Create FE k8s yaml
    - TODO: only http port 8030 open, other port(9020/9030/9011) failed to start, NEED FIX

--- README.md
+++ README.md
@@ -7,9 +7,6 @@
 # 创建namespace
 kubectl apply -f palo-ns.yaml
 
-# 创建PVC
-kubectl apply -f palo-pvc.yaml
-
 # 部署BE
 kubectl apply -f palo-be.yaml
 ```
---
+++ docker/be/bin/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Files created by Palo should always be group writable too
+umask 0002
+
+run_as_other_user_if_needed() {
+  if [[ "$(id -u)" == "0" ]]; then
+    # If running as root, drop to specified UID and run command
+    exec chroot --userspec=1000 / "${@}"
+  else
+    # Either we are running in Openshift with random uid and are a member of the root group
+    # or with a custom --user
+    exec "${@}"
+  fi
+}
+
+run_as_other_user_if_needed /usr/share/palo/be/bin/start_be.sh
